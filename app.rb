9664db5861008595a6368c51d24f89ea9804cd1e
haley swain <haleyswain@gmail.com>
2016-12-16 15:27:29 -0800

    edit erb pages to try and add stores to brands and brands to stores

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ require('./lib/brand')
 require('./lib/store')
 
 get('/') do
+  @brands = Brand.all()
+  @stores = Store.all()
   erb(:index)
 end
 
@@ -15,18 +17,37 @@ get('/stores') do
   erb(:stores)
 end
 
+get('/brands') do
+  @brands = Brand.all()
+  erb(:brands)
+end
+
 post('/stores') do
-  name = params.fetch('new-store')
+  name = params.fetch('name')
   store = Store.new(:name => name, :id => nil)
   store.save()
   @stores = Store.all()
   erb(:stores)
 end
 
+post('/brands') do
+  name = params.fetch('new-brand')
+  brand = Brand.new(:name => name, :id => nil)
+  brand.save()
+  @brands = Brand.all()
+  erb(:brands)
+end
+
 get('/stores/:id') do
   @store = Store.find(params.fetch('id').to_i())
   @brands = @store.brands()
   erb(:store)
+end
+
+get('/brands/:id') do
+  @brand = Brand.find(params.fetch('id').to_i())
+  @stores = @brand.stores()
+  erb(:brand)
 end
 
 patch('/stores/:id/edit') do
@@ -42,4 +63,33 @@ delete('/store/:id/delete') do
   @store.delete()
   @stores = Store.all()
   erb(:stores)
+end
+
+post('/store/:id') do
+  name = params.fetch('name')
+  store_id = params.fetch('store_id').to_i()
+  @store = Store.find(store_id.to_i())
+  @brands = Brand.new({:id => nil, :name => name, :store_id => store_id})
+  @brands.save()
+  @brands = @store.brands()
+  erb(:store)
+end
+
+
+patch('/brands/:id') do
+  brand_id = params.fetch('id').to_i()
+  @brand = Brand.find(brand_id)
+  store_ids = params.fetch('store_ids')
+  @brand.update({:store_ids => store_ids})
+  @stores = Store.all()
+  erb(:brand)
+end
+
+patch('/stores/:id') do
+  store_id = params.fetch('id').to_i()
+  @store = Store.find(store_id)
+  brand_ids = params.fetch('brand_ids')
+  @brands = Brand.new({:store_ids => [store1.id()]})
+  @brands = Brand.all()
+  erb(:brand)
 end
