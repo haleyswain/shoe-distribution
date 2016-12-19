2bb4644a3b2d160dfc8bb4596a7ca73a261bfbfa
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2016-12-18 17:29:57 -0800

    repair routing and erb files to be able to add shoe stores to brands with checkboxes

--- app.rb
+++ app.rb
@@ -5,7 +5,18 @@ also_reload("lib/**/*.rb")
 require("pg")
 require('./lib/brand')
 require('./lib/store')
+require('pry')
 
+# get '/things' do                      gets an array of things
+# get '/things/:id' do                  gets one thing by id
+# post '/things' do                     creates a new thing
+# patch '/things/:id' do                updates thing with given id
+# delete '/things/:id' do               deletes thing with given id
+
+# post '/things/:id/items'              creates an item and adds it to the thing with id
+# patch '/things/:thing_id/items/:id'   adds an existing item to the thing
+
+#
 get('/') do
   @brands = Brand.all()
   @stores = Store.all()
@@ -38,57 +49,69 @@ post('/brands') do
 end
 
 get('/stores/:id') do
-  @store = Store.find(params.fetch('id').to_i())
-  @brands = @store.brands()
+  @store = Store.find(params.fetch('id').to_i)
+  erb(:store)
+end
+
+
+get('/store/:id') do
+  @brands = Brand.all()
+  @store = Store.find(params.fetch('id').to_i)
   erb(:store)
 end
 
 get('/brands/:id') do
   @brand = Brand.find(params.fetch('id').to_i())
-  @stores = @brand.stores()
-  erb(:brand)
-end
-
-patch('/stores/:id/edit') do
-  name = params.fetch('edit-store')
-  @store = Store.find(params.fetch('id').to_i())
-  @store.update(:name => name)
-  @brands = @store.brands()
-  erb(:store)
-end
-
-delete('/store/:id/delete') do
-  @store = Store.find(params.fetch('id').to_i())
-  @store.delete()
-  @stores = Store.all()
-  erb(:stores)
-end
-
-post('/store/:id') do
-  name = params.fetch('name')
-  store_id = params.fetch('store_id').to_i()
-  @store = Store.find(store_id.to_i())
-  @brands = Brand.new({:id => nil, :name => name, :store_id => store_id})
-  @brands.save()
-  @brands = @store.brands()
-  erb(:store)
-end
-
-
-patch('/brands/:id') do
-  brand_id = params.fetch('id').to_i()
-  @brand = Brand.find(brand_id)
-  store_ids = params.fetch('store_ids')
-  @brand.update({:store_ids => store_ids})
   @stores = Store.all()
   erb(:brand)
 end
 
 patch('/stores/:id') do
-  store_id = params.fetch('id').to_i()
-  @store = Store.find(store_id)
-  @brand_ids = params.fetch('brand_ids')
-  @brands = Brand.new({:store_ids => [store1.id()]})
+  name = params.fetch('edit-store')
+  store = Store.find(params.fetch('id').to_i())
+  store.update(:name => name)
+  redirect '/'
+end
+
+delete('/stores/:id') do
+  @store = Store.find(params.fetch('id').to_i())
+  @store.delete()
+  @stores = Store.all()
+  redirect '/'
+end
+
+post('/store/:id') do
+  name = params.fetch('name')
   @brands = Brand.all()
-  erb(:brand)
+  @store = Store.find(params.fetch(store_id.to_i()))
+  new_brand = Brand.where({name: name})
+  # @store.brands.push(new_brand)
+  redirect '/'
+end
+
+post('/brands/:id') do
+  brand_id = params.fetch('id').to_i()
+  brand = Brand.find(brand_id)
+  store_ids = params.fetch('brand_ids')
+  store_ids.each do |store_id|
+    store = Store.find(store_id)
+    brand.stores.push(store)
+    brand.save
+  end
+  redirect '/'
+end
+
+patch('/stores/:id') do
+  brands = Brand.all()
+  brand_id = Brand.find(params.fetch('brand_id').to_i())
+  store = Store.find(params.fetch('id').to_i())
+  store.brands.push(@brand_id)
+  redirect '/'
+end
+
+post('/stores/:id/brands') do
+  store = Store.find(params.fetch("id").to_i)
+  brand = Brand.create({:name => params.fetch("name")})
+  store.brands.push(brand)
+  redirect '/'
 end
